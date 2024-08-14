684fdcfbd2ff5168732eebe110b8fdb77c1ddc34
Abdullah Abed <abedabdallah834@gmail.com>
2024-08-14 16:31:29 +0300

    first commit

---
+++ lib/core/Utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFruitAppIcon
+  /// assets/images/fruit_app_icon.png
+  static const String imagesFruitAppIcon = "assets/images/fruit_app_icon.png";
+
+  /// Assets for imagesFruitInbording2
+  /// assets/images/fruit_inbording2.svg
+  static const String imagesFruitInbording2 = "assets/images/fruit_inbording2.svg";
+}
+
